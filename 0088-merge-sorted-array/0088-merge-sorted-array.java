a10700023642a8b850c0d600694cbde36e08b14d
Dinesh Beniwal <dineshbeniwal1515@gmail.com>
2026-06-30 01:28:40 +0530

    Time: 3 ms (21.28%), Space: 44.3 MB (8.39%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.java
+++ 0088-merge-sorted-array/0088-merge-sorted-array.java
@@ -1,9 +1,9 @@
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-        int index=0;
-        for(int i=m;i<m+n;i++){
-            nums1[i]=nums2[index];
-            index++;
+    
+        for(int i=m,j=0;j<n;j++){
+            nums1[i]=nums2[j];
+            i++;
         }
         Arrays.sort(nums1);
     }
