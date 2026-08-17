d5c8d6b948c14672102bdb2bb4b32c19ee315d15
Dinesh Beniwal <dineshbeniwal1515@gmail.com>
2026-08-17 23:56:44 +0530

    Time: 2 ms (7.08%), Space: 48 MB (94.66%) - LeetHub

---
+++ 0344-reverse-string/0344-reverse-string.java
@@ -0,0 +1,14 @@
+class Solution {
+    public void reverseString(char[] s) {
+        int n=s.length;
+
+        StringBuilder sb = new StringBuilder();
+        for(int i=n-1;i>=0;i--){
+            sb.append(s[i]);
+        }
+        for(int i=0;i<n;i++){
+            s[i]=sb.charAt(i);
+        }
+    
+    }
+}
