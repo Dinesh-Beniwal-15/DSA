d6eaa093b38cafd02132730b7b8ec1855606b8cd
Dinesh Beniwal <dineshbeniwal1515@gmail.com>
2026-08-18 21:02:05 +0530

    Time: 8 ms (17.11%), Space: 47.6 MB (20.67%) - LeetHub

--- 0268-missing-number/0268-missing-number.java
+++ 0268-missing-number/0268-missing-number.java
@@ -1,22 +1,22 @@
 class Solution {
     public int missingNumber(int[] nums) {
-        // int n=nums.length;
+        int n=nums.length;
 
-        // Arrays.sort(nums);
+        Arrays.sort(nums);
         
-        // for(int i=0;i<n;i++){
-        //     if(i!=nums[i]){
-        //         return i;
+        for(int i=0;i<n;i++){
+            if(i!=nums[i]){
+                return i;
                 
-        //     }
-        // }
-        // return n;
+            }
+        }
+        return n;
 
        
-        int n = nums.length;
-        int Tsum = (n * (n + 1)) / 2;
-        int actualSum = Arrays.stream(nums).sum();
-        return Tsum - actualSum;
+        // int n = nums.length;
+        // int Tsum = (n * (n + 1)) / 2;
+        // int actualSum = Arrays.stream(nums).sum();
+        // return Tsum - actualSum;
        
 
         
