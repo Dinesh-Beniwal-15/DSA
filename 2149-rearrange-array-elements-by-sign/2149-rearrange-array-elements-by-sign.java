811e5907ce68e3cc91be7c0ea2c4a576b67524d2
Dinesh Beniwal <dineshbeniwal1515@gmail.com>
2026-08-29 13:30:07 +0530

    Time: 3 ms (100%), Space: 175.7 MB (28.58%) - LeetHub

--- 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.java
+++ 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.java
@@ -1,36 +1,50 @@
 class Solution {
     public int[] rearrangeArray(int[] nums) {
+        // int n=nums.length;
+        // int[] pos=new int[n/2];
+        // int[] neg=new int[n/2];
+        // int p1=0;
+        // int n1=0;
+        // for(int itr=0;itr<n;itr++){
+        //     if(nums[itr]>0){
+        //         pos[p1]=nums[itr];
+        //         p1++;
+        //     }
+        //     else{
+        //         neg[n1]=nums[itr];
+        //         n1++;
+        //     }
+        // }
+        // int p2=0;
+        // int n2=0;
+        // for(int i=0;i<n;i++){
+        //     if(i%2==0){
+        //         nums[i]=pos[p2];
+        //         p2++;
+        //     }
+        //     else{
+        //         nums[i]=neg[n2];
+        //         n2++;
+        //     }
+        // }
+        // return nums;
+
         int n=nums.length;
 
-        int[] pos=new int[n/2];
-        int[] neg=new int[n/2];
+        int [] result=new int[n];
 
-        int p1=0;
-        int n1=0;
-        for(int itr=0;itr<n;itr++){
-            if(nums[itr]>0){
-                pos[p1]=nums[itr];
-                p1++;
-            }
-            else{
-                neg[n1]=nums[itr];
-                n1++;
-            }
-        }
-
-        int p2=0;
-        int n2=0;
+        int pos=0,neg=1;
 
         for(int i=0;i<n;i++){
-            if(i%2==0){
-                nums[i]=pos[p2];
-                p2++;
+            if(nums[i]>0){
+                result[pos]=nums[i];
+                pos+=2;
             }
             else{
-                nums[i]=neg[n2];
-                n2++;
+                result[neg]=nums[i];
+                neg+=2;
             }
         }
-        return nums;
+        return result;
     }
 }
